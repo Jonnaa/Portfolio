e4034cb9e47158071dd22b51f6d52ef2c1642a28
Jonathan <Jonnaa96@yahoo.com>
2024-04-02 16:57:33 -0600

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="bg-black flex flex-row justify-between p-px">
+        <span className="invisible">Invis</span>
+        <span className="">Jonathan Navarro</span>
+        <nav className="">Burger</nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
