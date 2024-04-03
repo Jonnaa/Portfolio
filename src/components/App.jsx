88a4b57710ccb7e09b4bddca34c8b5b2c87b8814
Jonathan <Jonnaa96@yahoo.com>
2024-04-02 19:25:52 -0600

    Burger menu progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,92 @@ const App = () => {
   return (
     <>
       <Header />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores! Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam dolore! Quia, quibusdam.
+      <div className="">
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+          deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+          Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+          dolore! Quia, quibusdam.
+        </span>
+      </div>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+        Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+        dolore! Quia, quibusdam.
+      </span>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+        Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+        dolore! Quia, quibusdam.
+      </span>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+        Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+        dolore! Quia, quibusdam.
+      </span>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+        deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores!
+        Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam
+        dolore! Quia, quibusdam.
+      </span>
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import Burger from "../assets/burger-menu.svg";
 
 const Header = () => {
   const [burgerClicked, setBurgerClicked] = useState(false);
-  const navClasses = "bg-black flex-auto w-full text-center underline underline-offset-2 hover:bg-slate-700"
+  const navClasses =
+    "flex-auto w-full content-center text-center underline underline-offset-2 hover:bg-slate-700 text-4xl";
   function handleOnClick() {
     setBurgerClicked(!burgerClicked);
   }
   return (
     <>
-      <div className="p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+      <div className="sticky top-0 z-50 p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ">
         <div className="bg-black flex flex-row justify-between p-px">
           <span className="invisible">Invis</span>
           <span className="">Jonathan Navarro</span>
@@ -24,11 +25,17 @@ const Header = () => {
         </div>
       </div>
       {burgerClicked ? (
-        <nav className=" p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ">
-          <div className=" flex flex-col items-center">
-            <a href="#home" className={navClasses}>Home</a>
-            <a href="#aboutMe" className={navClasses}>About Me</a>
-            <a href="#projects" className={navClasses}>Projects</a>
+        <nav className="fixed w-full h-full z-40 overscroll-contain">
+          <div className="flex flex-col w-full h-full bg-black/75 backdrop-blur-sm">
+            <a href="#home" className={navClasses}>
+              Home
+            </a>
+            <a href="#aboutMe" className={navClasses}>
+              About Me
+            </a>
+            <a href="#projects" className={navClasses}>
+              Projects
+            </a>
           </div>
         </nav>
       ) : (
