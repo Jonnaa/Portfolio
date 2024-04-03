b4c6da5c2092ed2c2dd6856f8b013439fa53f8f0
Jonathan <Jonnaa96@yahoo.com>
2024-04-02 18:25:01 -0600

    Navbar hamburger onclick displays proper items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./Header";
 
-const App = () =>{
+const App = () => {
   return (
-    <Header/>
-  )
-}
+    <>
+      <Header />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat deleniti accusamus ea nam ipsa dolorum cumque quibusdam id dolores! Perspiciatis, maxime non eveniet culpa sequi veritatis laboriosam dolore! Quia, quibusdam.
+    </>
+  );
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
+import Burger from "../assets/burger-menu.svg";
+
 const Header = () => {
+  const [burgerClicked, setBurgerClicked] = useState(false);
+  const navClasses = "bg-black flex-auto w-full text-center underline underline-offset-2 hover:bg-slate-700"
+  function handleOnClick() {
+    setBurgerClicked(!burgerClicked);
+  }
   return (
-    <div className="p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-      <div className="bg-black flex flex-row justify-between p-px">
-        <span className="invisible">Invis</span>
-        <span className="">Jonathan Navarro</span>
-        <nav className="">Burger</nav>
+    <>
+      <div className="p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+        <div className="bg-black flex flex-row justify-between p-px">
+          <span className="invisible">Invis</span>
+          <span className="">Jonathan Navarro</span>
+          <nav className="">
+            <img
+              src={Burger}
+              alt="Navigation Menu"
+              className="w-6 rounded bg-gradient-to-b from-purple-400 via-pink-500 to-red-500 cursor-pointer"
+              onClick={handleOnClick}
+            />
+          </nav>
+        </div>
       </div>
-    </div>
+      {burgerClicked ? (
+        <nav className=" p-[2.2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ">
+          <div className=" flex flex-col items-center">
+            <a href="#home" className={navClasses}>Home</a>
+            <a href="#aboutMe" className={navClasses}>About Me</a>
+            <a href="#projects" className={navClasses}>Projects</a>
+          </div>
+        </nav>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
