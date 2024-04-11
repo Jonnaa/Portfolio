57951028b28d26e4dfd714afec61910e413c55ca
Jonathan <Jonnaa96@yahoo.com>
2024-04-11 17:41:02 -0600

    Added progress bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Burger from "../assets/burger-menu.svg";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { motion, useScroll} from "framer-motion";
 
 const Header = () => {
   const [burgerClicked, setBurgerClicked] = useState(false);
   const navClasses =
     "flex-auto w-full content-center text-center hover:underline hover:decoration-purple-400 hover:underline-offset-2 hover:bg-slate-700/50 hover:border-2 text-3xl hover:border-rose-500 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-purple-400 hover:via-pink-500 hover:to-red-500";
-  function handleOnClick() {
+  const {scrollYProgress} = useScroll();
+    function handleOnClick() {
     setBurgerClicked(!burgerClicked);
     if(burgerClicked){
       enablePageScroll()
@@ -31,6 +33,14 @@ const Header = () => {
           </nav>
         </div>
       </div>
+      <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          x:"-50%",
+          y:"-50%"
+        }}
+        className="fixed top-[50px] left-1/2 h-[6px] w-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 z-50"
+      />
       {burgerClicked ? (
         <nav className="fixed h-[calc(100vh-42px)] mt-12 w-full z-50 overscroll-contain">
           <div className="flex flex-col w-full h-full bg-black/75 backdrop-blur-sm">
