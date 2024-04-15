b81d291e406911aed5f3832fa6b48dea24bee977
Jonathan <Jonnaa96@yahoo.com>
2024-04-15 16:01:50 -0600

    Burger button animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
 import Burger from "../assets/burger-menu.svg";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
-import { motion, useScroll} from "framer-motion";
+import { motion, useScroll, useAnimate } from "framer-motion";
 
 const Header = () => {
   const [burgerClicked, setBurgerClicked] = useState(false);
+  const [scope, animate] = useAnimate();
   const navClasses =
     "flex-auto w-full content-center text-center hover:underline hover:decoration-purple-400 hover:underline-offset-2 hover:bg-slate-700/50 hover:border-2 text-3xl hover:border-rose-500 hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-purple-400 hover:via-pink-500 hover:to-red-500";
-  const {scrollYProgress} = useScroll();
+  const { scrollYProgress } = useScroll();
 
   function handleOnClick() {
     setBurgerClicked(!burgerClicked);
-    burgerClicked ? enablePageScroll() : disablePageScroll()
+    if (burgerClicked) {
+      enablePageScroll();
+      handleAnimate(false);
+    } else {
+      handleAnimate(true);
+      disablePageScroll();
+    }
   }
+
+  const handleAnimate = async (a) => {
+    a?
+    await animate("#burgerButton", { rotate: "90deg" })
+    :
+    await animate("#burgerButton", { rotate: "0deg" })
+  };
 
   return (
     <>
       <div className="fixed w-full top-0 z-40 p-[2.8px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 ">
-        <div className="bg-black flex flex-row justify-between p-px">
+        <div
+          ref={scope}
+          className="bg-black flex flex-row justify-between p-px"
+        >
           <span className="invisible"></span>
           <nav>
             <img
               src={Burger}
               alt="Navigation Menu"
+              id="burgerButton"
               className="w-10 rounded  hover:bg-red-500 cursor-pointer z-50 bg-white"
               onClick={handleOnClick}
             />
@@ -32,8 +50,8 @@ const Header = () => {
       <motion.div
         style={{
           scaleX: scrollYProgress,
-          x:"-50%",
-          y:"-50%"
+          x: "-50%",
+          y: "-50%",
         }}
         className="fixed top-[49px] left-1/2 h-[8px] w-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 z-50"
       />
