126410e2545be2019fd03882ead6575b4d158bb3
Jonathan <Jonnaa96@yahoo.com>
2024-04-17 16:17:39 -0600

    Projects page basic layout

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,20 +4,26 @@ const Projects = () => {
       <h1 className="text-4xl sticky top-1 z-40 w-96 mx-auto text-center">
         Projects
       </h1>
-      {/* Div holds all projects */}
-      <div className="m-8">
-        {/* Project 1 Div */}
-        <div>
-          <h2>Tempo</h2>
+      {/* Projects page contents */}
+      <div className="mx-4 my-7 min-w-[calc(90vh)] min-h-[calc(90vh)] border relative flex justify-between flex-col py-[60px]">
+        {/* Div holds projects - small versions */}
+        <div className="flex justify-between w-4/5 gap-4 mx-auto relative">
+          {/* Project 1 Div */}
+          <div className="w-1/3 border cursor-pointer min-h-[calc(20vh)]">
+            <h2>Tempo</h2>
+            <div></div>
+          </div>
+          {/* Project 2 Div */}
+          <div className="w-1/3 border cursor-pointer">
+            <h2>KDramas</h2>
+          </div>
+          {/* Project 3 Div */}
+          <div className="w-1/3 border cursor-pointer">
+            <h2>KIT</h2>
+          </div>
         </div>
-        {/* Project 2 Div */}
-        <div>
-          <h2>KDramas</h2>
-        </div>
-        {/* Project 3 Div */}
-        <div>
-          <h2>KIT</h2>
-        </div>
+        {/* Large version of the project selected, starts as a placeholder */}
+        <div className="border w-4/5 min-h-[calc(50vh)] mx-auto">placeholder</div>
       </div>
     </div>
   );
