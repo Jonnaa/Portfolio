7cfa3390db4bcfec5eeaa39005ed4bfddb2e53e2
Jonathan <Jonnaa96@yahoo.com>
2024-04-17 18:04:57 -0600

    created a useState hook and a function to keep track of which project to display

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,89 @@
+import { useState } from "react";
+
 const Projects = () => {
-  const tempoImgs = [
-    {id:1,location:"tempoScreenshots/Home.jpg",name:"Home"},
-    {id:2,location:"tempoScreenshots/ArtistIndex.jpg",name:"Artist Index"},
-    {id:3,location:"tempoScreenshots/ArtistDetail.jpg",name:"Artist Detail"},
-    {id:4,location:"tempoScreenshots/Player.jpg",name:"Player"},
-    {id:5,location:"tempoScreenshots/MerchIndex.jpg",name:"Merch Index"},
-    {id:6,location:"tempoScreenshots/MerchDetail.jpg",name:"Merch Detail"},
-  ]
+    const tempoImgs = [
+    { id: 1, location: "tempoScreenshots/Home.jpg", name: "Home" },
+    {
+      id: 2,
+      location: "tempoScreenshots/ArtistIndex.jpg",
+      name: "Artist Index",
+    },
+    {
+      id: 3,
+      location: "tempoScreenshots/ArtistDetail.jpg",
+      name: "Artist Detail",
+    },
+    { id: 4, location: "tempoScreenshots/Player.jpg", name: "Player" },
+    { id: 5, location: "tempoScreenshots/MerchIndex.jpg", name: "Merch Index" },
+    {
+      id: 6,
+      location: "tempoScreenshots/MerchDetail.jpg",
+      name: "Merch Detail",
+    },
+  ];
   const kdramasImgs = [
-    {id:1,location:"kdramasScreenshots/Home Page.jpg",name:"Home"},
-    {id:2,location:"kdramasScreenshots/Reviews.jpg",name:"Artist Index"},
-    {id:3,location:"kdramasScreenshots/Review Details.jpg",name:"Artist Detail"},
-    {id:4,location:"kdramasScreenshots/Edit Review Page.jpg",name:"Player"},
-    {id:5,location:"kdramasScreenshots/Watchlist Page.jpg",name:"Merch Index"},
-  ]
+    { id: 1, location: "kdramasScreenshots/Home Page.jpg", name: "Home" },
+    { id: 2, location: "kdramasScreenshots/Reviews.jpg", name: "Artist Index" },
+    {
+      id: 3,
+      location: "kdramasScreenshots/Review Details.jpg",
+      name: "Artist Detail",
+    },
+    {
+      id: 4,
+      location: "kdramasScreenshots/Edit Review Page.jpg",
+      name: "Player",
+    },
+    {
+      id: 5,
+      location: "kdramasScreenshots/Watchlist Page.jpg",
+      name: "Merch Index",
+    },
+  ];
 
   const kitImgs = [
-    {id:1,location:"public/kitScreenshots/Landing Page.jpg",name:"Home"},
-    {id:2,location:"public/kitScreenshots/Landing Page -Logged in.jpg",name:"Artist Index"},
-    {id:3,location:"public/kitScreenshots/Login.jpg",name:"Artist Detail"},
-    {id:4,location:"public/kitScreenshots/Create Post.jpg",name:"Player"},
-    {id:5,location:"public/kitScreenshots/Post Details.jpg",name:"Merch Index"},
-    {id:6,location:"public/kitScreenshots/Updated Landing Page.jpg",name:"Merch Index"},
-  ]
+    { id: 1, location: "public/kitScreenshots/Landing Page.jpg", name: "Home" },
+    {
+      id: 2,
+      location: "public/kitScreenshots/Landing Page -Logged in.jpg",
+      name: "Artist Index",
+    },
+    {
+      id: 3,
+      location: "public/kitScreenshots/Login.jpg",
+      name: "Artist Detail",
+    },
+    {
+      id: 4,
+      location: "public/kitScreenshots/Create Post.jpg",
+      name: "Player",
+    },
+    {
+      id: 5,
+      location: "public/kitScreenshots/Post Details.jpg",
+      name: "Merch Index",
+    },
+    {
+      id: 6,
+      location: "public/kitScreenshots/Updated Landing Page.jpg",
+      name: "Merch Index",
+    },
+  ];
+  
+  const [projDisplay, setProjDisplay] = useState("0");
+
+  function switchDisplay(val) {
+    if (val === "1") {
+      setProjDisplay("1");
+    } else if (val === "2") {
+      setProjDisplay("2");
+    } else if (val === "3") {
+      setProjDisplay("3");
+    } else {
+      setProjDisplay("0");
+    }
+  }
+
 
   return (
     <div className="min-h-[calc(100vh)]" id="projects">
@@ -34,21 +95,36 @@ const Projects = () => {
         {/* Div holds projects - small versions */}
         <div className="flex justify-between w-4/5 gap-4 mx-auto relative">
           {/* Project 1 Div */}
-          <div className="w-1/3 border cursor-pointer min-h-[calc(20vh)]">
+          <div
+            className="w-1/3 border cursor-pointer min-h-[calc(20vh)]"
+            onClick={() => {
+              switchDisplay("1");
+            }}
+          >
             <h2>Tempo</h2>
             <div>
               <img src={tempoImgs[1].location} alt="" />
             </div>
           </div>
           {/* Project 2 Div */}
-          <div className="w-1/3 border cursor-pointer">
+          <div
+            className="w-1/3 border cursor-pointer"
+            onClick={() => {
+              switchDisplay("2");
+            }}
+          >
             <h2>KDramas</h2>
             <div>
               <img src={kdramasImgs[0].location} alt="" />
             </div>
           </div>
           {/* Project 3 Div */}
-          <div className="w-1/3 border cursor-pointer">
+          <div
+            className="w-1/3 border cursor-pointer"
+            onClick={() => {
+              switchDisplay("3");
+            }}
+          >
             <h2>KIT</h2>
             <div>
               <img src={kitImgs[5].location} alt="" />
@@ -56,7 +132,10 @@ const Projects = () => {
           </div>
         </div>
         {/* Large version of the project selected, starts as a placeholder */}
-        <div className="border w-4/5 min-h-[calc(50vh)] mx-auto">placeholder</div>
+        <div className="border w-4/5 min-h-[calc(50vh)] mx-auto">
+          <span className="text-center">placeholder</span>
+          <h2 className="text-center">{projDisplay}</h2>
+        </div>
       </div>
     </div>
   );
