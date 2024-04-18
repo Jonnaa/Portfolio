03b5907a9ac26f85e100687cc8ef987ebfad0e56
Jonathan <Jonnaa96@yahoo.com>
2024-04-18 17:12:11 -0600

    Changed all projects to same display method(Scroll horizontal to view images, no longer requies you to click buttons for prev and next

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -64,9 +64,6 @@ const Projects = () => {
     },
   ];
 
-  const [currentTempoImg, setCurrentTempoImg] = useState(0);
-  const [currentKdramasImg, setCurrentKdramasImg] = useState(0);
-  const [currentKitImg, setCurrentKitImg] = useState(0);
   const [projDisplay, setProjDisplay] = useState(0);
 
   function switchDisplay(val) {
@@ -87,64 +84,6 @@ const Projects = () => {
     }
   }
 
-  // onclick handler for prev button
-  function handlePrev(event) {
-    let projName = event.target.className;
-    projName = projName.substring(0, projName.indexOf(" "));
-    switch (projName) {
-      case "tempo":
-        if (currentTempoImg >= 1) {
-          setCurrentTempoImg(currentTempoImg - 1);
-        } else {
-          setCurrentTempoImg(tempoImgs.length - 1);
-        }
-        break;
-      case "kdramas":
-        if (currentKdramasImg >= 1) {
-          setCurrentKdramasImg(currentKdramasImg - 1);
-        } else {
-          setCurrentKdramasImg(kdramasImgs.length - 1);
-        }
-        break;
-      case "kit":
-        if (currentKitImg >= 1) {
-          setCurrentKitImg(currentKitImg - 1);
-        } else {
-          setCurrentKitImg(kitImgs.length - 1);
-        }
-        break;
-    }
-  }
-
-  // onclick handler for next button
-  function handleNext(event) {
-    let projName = event.target.className;
-    projName = projName.substring(0, projName.indexOf(" "));
-    switch (projName) {
-      case "tempo":
-        if (currentTempoImg < tempoImgs.length - 1) {
-          setCurrentTempoImg(currentTempoImg + 1);
-        } else {
-          setCurrentTempoImg(0);
-        }
-        break;
-      case "kdramas":
-        if (currentKdramasImg < kdramasImgs.length - 1) {
-          setCurrentKdramasImg(currentKdramasImg + 1);
-        } else {
-          setCurrentKdramasImg(0);
-        }
-        break;
-      case "kit":
-        if (currentKitImg < kitImgs.length - 1) {
-          setCurrentKitImg(currentKitImg + 1);
-        } else {
-          setCurrentKitImg(0);
-        }
-        break;
-    }
-  }
-
   // Variable that displays detailed version of whichever project is clicked
   let currentProject = (
     <h1 className="w-full text-center pt-44 text-4xl animate-pulse">
@@ -158,19 +97,6 @@ const Projects = () => {
         <h1 className="w-full flex flex-col justify-center text-center text-2xl">
           Tempo
         </h1>
-        {/* <div className="relative flex justify-center w-full h-full">
-          <button onClick={handlePrev} type="button" id="tempo" className="tempo w-1/12 border">
-            Prev
-          </button>
-          <img
-            src={tempoImgs[currentTempoImg].location}
-            alt=""
-            className="w-6/12"
-          />
-          <button onClick={handleNext} type="button" className="tempo w-1/12 border">
-            Next
-          </button>
-        </div> */}
         <div className="snap-x snap-mandatory flex flex-nowrap overflow-x-auto w-1/2 mx-auto">
           {tempoImgs.map((tempoImg) => (
             <div
@@ -190,20 +116,17 @@ const Projects = () => {
         <h1 className="w-full flex flex-col justify-center text-center text-2xl">
           KDramas
         </h1>
-        <div className="relative flex justify-center">
-          <button onClick={handlePrev} className="kdramas w-1/12 border">
-            Prev
-          </button>
-          <img
-            src={kdramasImgs[currentKdramasImg].location}
-            alt=""
-            className="w-6/12"
-          />
-          <button onClick={handleNext} className="kdramas w-1/12 border">
-            Next
-          </button>
+        <div className="snap-x snap-mandatory flex flex-nowrap overflow-x-auto w-1/2 mx-auto">
+          {kdramasImgs.map((kdramasImg) => (
+            <div
+              key={kdramasImg.id}
+              className="snap-start snap-always shrink-0 w-full"
+            >
+              <img src={kdramasImg.location} alt={kdramasImg.name}/>
+            </div>
+          ))}
         </div>
-        <div>Desc</div>
+        <div>GitHub Website</div>
       </div>
     );
   } else if (projDisplay === 3) {
@@ -212,20 +135,17 @@ const Projects = () => {
         <h1 className="w-full flex flex-col justify-center text-center text-2xl">
           KIT
         </h1>
-        <div className="relative flex justify-center">
-          <button onClick={handlePrev} className="kit w-1/12 border">
-            Prev
-          </button>
-          <img
-            src={kitImgs[currentKitImg].location}
-            alt=""
-            className="w-6/12"
-          />
-          <button onClick={handleNext} className="kit w-1/12 border">
-            Next
-          </button>
+        <div className="snap-x snap-mandatory flex flex-nowrap overflow-x-auto w-1/2 mx-auto">
+          {kitImgs.map((kitImg) => (
+            <div
+              key={kitImg.id}
+              className="snap-start snap-always shrink-0 w-full"
+            >
+              <img src={kitImg.location} alt={kitImg.name}/>
+            </div>
+          ))}
         </div>
-        <div>Desc</div>
+        <div>GitHub Website</div>
       </div>
     );
   }
