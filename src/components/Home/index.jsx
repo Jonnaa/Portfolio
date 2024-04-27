5a99b39188efd6949e9d1cc35e59d5bf0425aa20
Jonathan <Jonnaa96@yahoo.com>
2024-04-27 14:20:10 -0600

    Uncommented load in animation for home screen

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -8,19 +8,20 @@ const Home = () => {
     offset: ["start start","end start"]
   })
 
-  const opacity = useTransform(scrollYProgress, [0,.2],[1,0])
+  const opacity = useTransform(scrollYProgress, [0,.55],[1,0])
   return (
     <div className="min-h-screen snap-center bg-scroll" id="home">
       <h1 className="font-semibold text-3xl sm:text-4xl w-40 sm:w-56 mx-auto text-center sticky top-1 z-40">
         Home
       </h1>
       <motion.div
+      ref={targetRef}
         className="flex flex-col justify-center h-screen text-center"
         style={{opacity}}
-        // initial={{ opacity: 0, y:-50, scale: .5 }}
-        // animate={{ opacity: 1, y:0, scale:1}}
-        // viewport={{ amount: "all", once: true }}
-        // transition={{ delay: .25, duration:.5 }}
+        initial={{ opacity: 0, y:-50, scale: .5 }}
+        animate={{ opacity: 1, y:0, scale:1}}
+        viewport={{ amount: "all", once: true }}
+        transition={{ delay: .25, duration:.5 }}
       >
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold">Jonathan Navarro</h1>
         <p className="text-2xl sm:text-3xl md:text-4xl pt-12">Software Engineer</p>
