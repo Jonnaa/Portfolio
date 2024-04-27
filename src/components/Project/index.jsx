890f6d51cba9b37b6eedb0d828b209ade7759ddf
Jonathan <Jonnaa96@yahoo.com>
2024-04-27 15:39:40 -0600

    Added box shadows

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -2,7 +2,7 @@ const Project = ({ info }) => {
   const {name, gh, web, desc, imgs} = info
   return (
     <>
-      <div className="rounded bg-black h-full relative flex flex-col justify-between items-center">
+      <div className="rounded-lg bg-black h-full relative flex flex-col justify-between items-center">
         <h1 className="w-full flex flex-col justify-center text-center text-xl md:text-2xl">
           {name}
         </h1>
@@ -12,16 +12,16 @@ const Project = ({ info }) => {
               key={img.id}
               className="snap-start snap-always shrink-0 w-full max-w-[700px]"
             >
-              <img src={img.location} alt={img.name} className="rounded-t"/>
+              <img src={img.location} alt={img.name} className="rounded-t-lg"/>
             </div>
           ))}
         </div>
         <div className="w-3/4 sm:w-1/2 my-1">{desc}</div>
         <div className="flex justify-center gap-10 text-sm md:text-md mb-1">
-          <a href={gh} target="_blank" className="hover:bg-purple-400 bg-rose-200 text-black rounded p-1">
+          <a href={gh} target="_blank" className="bg-rose-200 shadow-md shadow-rose-200/50 hover:shadow-lg hover:bg-purple-400 hover:shadow-purple-400/50 text-black rounded-lg p-1">
             GitHub
           </a>
-          <a href={web} target="_blank" className="hover:bg-purple-400 bg-rose-200 text-black rounded p-1">
+          <a href={web} target="_blank" className="bg-rose-200 shadow-md shadow-rose-200/50 hover:shadow-lg hover:bg-purple-400 hover:shadow-purple-400/50 text-black rounded-lg p-1">
             Website
           </a>
         </div>
