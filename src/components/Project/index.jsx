4b4e36ac5e7d776856ed1ea3cbd63c7214a563a6
Jonathan <Jonnaa96@yahoo.com>
2024-04-27 13:18:36 -0600

    Changed project buttons | Rounded corners

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -2,8 +2,8 @@ const Project = ({ info }) => {
   const {name, gh, web, desc, imgs} = info
   return (
     <>
-      <div className="bg-black h-full relative flex flex-col justify-between items-center">
-        <h1 className="w-full flex flex-col justify-center text-center text-lg md:text-2xl">
+      <div className="rounded bg-black h-full relative flex flex-col justify-between items-center">
+        <h1 className="w-full flex flex-col justify-center text-center text-xl md:text-2xl">
           {name}
         </h1>
         <div className="snap-x snap-mandatory flex flex-nowrap overflow-x-auto w-full mx-auto max-w-[700px]">
@@ -12,16 +12,16 @@ const Project = ({ info }) => {
               key={img.id}
               className="snap-start snap-always shrink-0 w-full max-w-[700px]"
             >
-              <img src={img.location} alt={img.name} />
+              <img src={img.location} alt={img.name} className="rounded-t"/>
             </div>
           ))}
         </div>
         <div className="w-3/4 sm:w-1/2">{desc}</div>
-        <div className="flex justify-center gap-10 text-sm md:text-md">
-          <a href={gh} target="_blank" className="hover:text-purple-600 visited:text-pink-300">
+        <div className="flex justify-center gap-10 text-sm md:text-md mb-1">
+          <a href={gh} target="_blank" className="hover:bg-purple-400 bg-rose-200 text-black rounded p-1">
             GitHub
           </a>
-          <a href={web} target="_blank" className="hover:text-purple-600 visited:text-pink-300">
+          <a href={web} target="_blank" className="hover:bg-purple-400 bg-rose-200 text-black rounded p-1">
             Website
           </a>
         </div>
