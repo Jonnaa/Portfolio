26bc18c689504b45f01bb0632f103efdd7ca52a5
Jonathan <Jonnaa96@yahoo.com>
2024-04-30 11:45:17 -0600

    Changed resume button theme to match linkedin button

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -16,11 +16,13 @@ const Footer = () => {
           <motion.a
             onHoverStart={() => setResumeButton("Resume")}
             onHoverEnd={() => setResumeButton("R")}
-            href="https://docs.google.com/document/d/10ivZXeMhmoGI26BLo_NN6eRzhNStdY4wbLfrHfhqQtQ/edit?usp=sharing"
-            className="w-10 h-10 sm:w-16 hover:w-24 sm:hover:w-32 sm:h-16 bg-purple-300 shadow-md shadow-purple-300/50 hover:shadow-lg hover:bg-purple-500 hover:shadow-purple-500/50 text-black rounded-lg p-1 text-3xl sm:text-5xl hover:text-xl sm:hover:text-3xl font-bold flex justify-center items-center overflow-hidden transition-all"
+            whileTap={() => setResumeButton("Resume")}
+            className="w-10 h-10 sm:w-16 hover:w-24 sm:hover:w-32 sm:h-16 bg-purple-300 shadow-md shadow-purple-300/50 hover:shadow-lg hover:bg-purple-500 hover:shadow-purple-500/50 text-black rounded-lg p-1 text-3xl sm:text-5xl hover:text-xl sm:hover:text-2xl font-bold overflow-hidden transition-all"
             target="_blank"
           >
-            {resumeButton}
+            <div className="bg-black rounded-lg text-purple-300 hover:text-purple-500 w-full h-full flex justify-center items-center">
+              {resumeButton}
+            </div>
           </motion.a>
         </div>
   )
