c7a3c9a89d12a190017e498d083737f8b4bade7d
Jonathan <Jonnaa96@yahoo.com>
2024-04-30 12:14:52 -0600

    Responsiveness tweaked

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -3,26 +3,18 @@ import { useState } from "react";
 
 const Project = ({ info, imgIndex, setImgIndex }) => {
   const { name, gh, web, desc, imgs } = info;
-  // dragging state that is set using framer motion ondragstart and ondragEnd
   const [dragging, setDragging] = useState(false);
-  // Img index state that will help paginate through the images
-  //  (Once an image is dragged far enough to either side, then inc or dec img indx
-  //  which is then multiplied by 100 and translatesX -100 or 100 from current value)
-  // const [imgIndex, setImgIndex] = useState(0)
-  // motion value from framer motion will be used to see how far we have dragged an image
   const dragX = useMotionValue(0);
 
   const dragBuffer = 20;
-  // ondrag functions that handle framer motion ondragstart and ondragend events
+
   const onDragStart = () => {
     setDragging(true);
   };
+
   const onDragEnd = () => {
     setDragging(false);
-    // Checking motion value for testing
-    // console.log(dragX.get())
     const x = dragX.get();
-
     if (x <= -dragBuffer) {
       handleNext()
     } else if (x >= dragBuffer) {
@@ -31,18 +23,11 @@ const Project = ({ info, imgIndex, setImgIndex }) => {
   };
 
   const handleNext=()=>{
-    if (imgIndex === imgs.length - 1) {
-      setImgIndex(0);
-    } else {
-      setImgIndex(imgIndex + 1);
-    }
+    imgIndex === imgs.length-1 ? setImgIndex(0):setImgIndex(imgIndex + 1)
   }
+
   const handlePrev = ()=>{
-    if (imgIndex === 0) {
-      setImgIndex(imgs.length-1);
-    } else {
-      setImgIndex(imgIndex - 1);
-    }
+    imgIndex === 0?setImgIndex(imgs.length-1):setImgIndex(imgIndex - 1)
   }
 
   return (
@@ -95,22 +80,22 @@ const Project = ({ info, imgIndex, setImgIndex }) => {
               />
             ))}
           </motion.div>
-          <button className="absolute left-1 top-[40%] bg-black hover:bg-purple-500 hover:shadow-purple-500/50 shadow-md shadow-black rounded-lg p-1 font-bold text-lg" onClick={handlePrev}>&larr;</button>
-          <button className="absolute right-1 top-[40%] bg-black hover:bg-purple-500 hover:shadow-purple-500/50 shadow-md shadow-black rounded-lg p-1" onClick={handleNext}>&rarr;</button>
+          <button className="absolute left-1 top-[35%] sm:top-[40%] bg-black hover:bg-purple-500 hover:shadow-purple-500/50 shadow-md shadow-black rounded-lg p-1 font-bold text-lg" onClick={handlePrev}>&larr;</button>
+          <button className="absolute right-1 top-[35%] sm:top-[40%] bg-black hover:bg-purple-500 hover:shadow-purple-500/50 shadow-md shadow-black rounded-lg p-1" onClick={handleNext}>&rarr;</button>
         </div>
         <div className="w-3/4 sm:w-1/2 my-1 text-sm md:text-base ">{desc}</div>
         <div className="flex justify-center gap-10 text-sm md:text-base mb-1 text-center">
           <a
             href={gh}
             target="_blank"
-            className="w-16 sm:w-20 bg-purple-300 shadow-md shadow-purple-300/50 hover:shadow-lg hover:bg-purple-500 hover:shadow-purple-500/50 text-black rounded-lg p-1 transition-all"
+            className="w-16 sm:w-20 bg-purple-300 shadow-md shadow-purple-300/50 hover:shadow-lg hover:bg-purple-500 hover:shadow-purple-500/50 text-black rounded-lg sm:text-lg p-1 sm:p-0 transition-all"
           >
             GitHub
           </a>
           <a
             href={web}
             target="_blank"
-            className="w-16 sm:w-20 bg-purple-300 shadow-md shadow-purple-300/50 hover:shadow-lg hover:bg-purple-500 hover:shadow-purple-500/50 text-black rounded-lg p-1 transition-all"
+            className="w-16 sm:w-20 bg-purple-300 shadow-md shadow-purple-300/50 hover:shadow-lg hover:bg-purple-500 hover:shadow-purple-500/50 text-black rounded-lg sm:text-lg p-1 sm:p-0 transition-all"
           >
             Website
           </a>
